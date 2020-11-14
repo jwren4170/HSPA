68288537ae1c0ecacda11d57147f199a0b7697f9
JWord <jwren4170@att.net>
2020-11-13 18:28:01 -0600

    Added property-list with @Input from property-card

--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
+  properties: Array<any> = [
+    {
+      Id: 1,
+      Name: 'Birla House',
+      Type: 'House',
+      Price: '120,000',
+    },
+    {
+      Id: 2,
+      Name: 'Erose Villa',
+      Type: 'House',
+      Price: '140,000',
+    },
+    {
+      Id: 3,
+      Name: 'Macro Home',
+      Type: 'House',
+      Price: '220,000',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
